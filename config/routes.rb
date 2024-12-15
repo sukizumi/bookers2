eb8999ed6470031bee2ddd96935b3fcdec8e01be
sukizumi <187249056+sukizumi@users.noreply.github.com>
2024-12-15 06:38:20 +0000

    11章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/about' => 'homes#about', as: 'about'
+
+  resources :books, only: [:new, :create, :index, :show]
+
 end
