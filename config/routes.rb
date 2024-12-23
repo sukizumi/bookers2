b35f057685bcaf22deab81661d71f69e0f03c7af
sukizumi <187249056+sukizumi@users.noreply.github.com>
2024-12-23 09:05:31 +0000

    Failure 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
+  root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
 
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
